6c0d68f5afea6958df6dc738a70c60f2aff95bc6
mic <mic@micsapp.com>
2025-05-19 21:24:18 -0400

    Add hello_world.py and requirements.txt

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+"""
+Simple Hello World program to demonstrate basic Python functionality
+"""
+
+def main():
+    """Main function that prints a welcome message"""
+    print("Hello, World!")
+    print("欢迎来到 Python 世界!")  # Chinese welcome message
+
+if __name__ == "__main__":
+    main()
